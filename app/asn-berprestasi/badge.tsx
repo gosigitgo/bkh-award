500b7fa36476df931c7d7e91e6d4bfe95cdff18c
gosigitgo <sgt.wibowo@gmail.com>
2023-09-01 04:51:37 +0700

    commit 1 sept 2023

--- app/asn-berprestasi/badge.tsx
+++ app/asn-berprestasi/badge.tsx
@@ -18,9 +18,9 @@ export default function Badge() {
                     </p>
                     <p className="text-sm font-bold pt-5">1. <i>Inspiring Leader</i>&nbsp;&nbsp;<span className="text-xs text-rose-400">(Maks. 1 pegawai)</span></p>
                     <p className="text-sm">Penghargaan ini diberikan kepada <strong>Pejabat Pimpinan Tinggi Pratama/yang disetarakan/Kepala Satuan Kerja</strong> yang mampu menginspirasi, memotivasi ASN melalui karya, kinerja, dan perilaku kepemimpinan.</p>
-                    <p className="text-sm font-bold pt-3">2. <i>Future Leader</i>&nbsp;&nbsp;<span className="text-xs text-rose-400">(Maks. 1 pegawai)</span></p>
+                    <p className="text-sm font-bold pt-3">2. <i>Future Leader</i></p>
                     <p className="text-sm">Penghargaan ini diberikan kepada <strong>Pejabat Administrasi atau Pejabat Fungsional yang memiliki tugas tambahan sebagai Ketua Tim Kerja / Ketua Task Force / Koordinator / Subkoordinator</strong> berusia maksimal 45 tahun yang dinilai memiliki perilaku kepemimpinan yang adaptif, berorientasi kinerja dan visioner.</p>
-                    <p className="text-sm font-bold pt-3">3. <i>Best Innovator</i>&nbsp;&nbsp;<span className="text-xs text-rose-400">(Maks. 1 pegawai)</span></p>
+                    <p className="text-sm font-bold pt-3">3. <i>Best Innovator</i></p>
                     <p className="text-sm">Penghargaan ini diberikan kepada <strong>Pegawai ASN baik individu maupun tim</strong> yang memiliki karya yang baru, orisinal, dan/atau karya hasil pengembangan yang memiliki nilai lebih tinggi dari karya sebelumnya, yang bermanfaat bagi diri sendiri, rekan kerja, unit kerja, instansi, dan masyarakat.</p>
                     <p className="pt-4 text-sm flex"><CalendarDaysIcon width={20} height={20} className="mr-2" /> Batas Pendaftaran adalah tanggal <strong className="text-red-600">&nbsp;22 September 2023</strong></p>
                     <Panduan/>
