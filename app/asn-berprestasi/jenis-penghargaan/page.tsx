9272eb05629551c15e8b88ce23967a6d7e0ad6a8
gosigitgo <sgt.wibowo@gmail.com>
2023-08-31 23:14:58 +0700

    deploy 31 Agustus 2023

--- app/asn-berprestasi/jenis-penghargaan/page.tsx
+++ app/asn-berprestasi/jenis-penghargaan/page.tsx
@@ -5,12 +5,12 @@ import PilihJenis from "./pilih_jenis";
 import Pilih from "./button_pilih";
 import { useState } from "react";
 
-export const metadata = {
-    title: '.: ASN Berprestasi | Penghargaan Bhakti Karya Husada :.',
-    description: 'Aplikasi Penghargaan Bhakti Karya Husada',
-    developer: 'sgt.wibowo@gmail.com',
-    icons: '/images/favicon.png'
-}
+// export const metadata = {
+//     title: '.: ASN Berprestasi | Penghargaan Bhakti Karya Husada :.',
+//     description: 'Aplikasi Penghargaan Bhakti Karya Husada',
+//     developer: 'sgt.wibowo@gmail.com',
+//     icons: '/images/favicon.png'
+// }
 
 export default function Page() {
     const [pilih, setPilih] = useState(null)
