690cd26107014b7a63552ace7b9fe6b443d4d8a6
gosigitgo <sgt.wibowo@gmail.com>
2023-09-01 04:45:08 +0700

    commit 1 sept 2023

--- app/asn-berprestasi/panduan.tsx
+++ app/asn-berprestasi/panduan.tsx
@@ -21,7 +21,7 @@ export default function Panduan() {
 
     return (
         <div className="flex flex-wrap gap-1 mt-2">
-        <div className="py-1 px-2 bg-green-500 rounded-md text-center font-bold text-white text-sm cursor-pointer hover:bg-green-600" onClick={handleClick} >PENGUMUMAN LENGKAP</div>
+        <a className="py-1 px-2 bg-green-500 rounded-md text-center font-bold text-white text-sm cursor-pointer hover:bg-green-600" href="formulir/Pengumuman_Pelaksanaan_Seleksi_ASN_Berprestasi_Tahun_2023.pdf" target='_blank' >PENGUMUMAN LENGKAP</a>
         <div className="py-1 px-2 bg-blue-500 rounded-md text-center font-bold text-white text-sm cursor-pointer hover:bg-blue-600" onClick={handleClick2} >DAFTAR USULAN SATKER</div>
         <ModalDownload/>
         </div>
