3f313f5f31f6e4de9a658c5538e5ba018e6d53b5
gosigitgo <sgt.wibowo@gmail.com>
2023-08-31 23:23:25 +0700

    deploy 31 agustus 2023

--- app/asn-berprestasi/page.tsx
+++ app/asn-berprestasi/page.tsx
@@ -1,12 +1,12 @@
 import Badge from "./badge"; 
 import Daftar from "./button_daftar";
 
-// export const metadata = {
-//     title: '.: ASN Berprestasi | Penghargaan Bhakti Karya Husada :.',
-//     description: 'Aplikasi Penghargaan Bhakti Karya Husada',
-//     developer: 'sgt.wibowo@gmail.com',
-//     icons: '/images/favicon.png'
-// }
+export const metadata = {
+    title: '.: ASN Berprestasi | Penghargaan Bhakti Karya Husada :.',
+    description: 'Aplikasi Penghargaan Bhakti Karya Husada',
+    developer: 'sgt.wibowo@gmail.com',
+    icons: '/images/favicon.png'
+}
 
 export default function Page() {
     return (
--- app/asn-berprestasi/pilih-pegawai/page.tsx
+++ app/asn-berprestasi/pilih-pegawai/page.tsx
@@ -5,12 +5,12 @@ import { useState } from "react";
 import InputSearchFutureLeader from "./search_future_leader";
 import InputSearchBestInnovator from "./search_best_innovator";
 
-export const metadata = {
-    title: '.: ASN Berprestasi | Penghargaan Bhakti Karya Husada :.',
-    description: 'Aplikasi Penghargaan Bhakti Karya Husada',
-    developer: 'sgt.wibowo@gmail.com',
-    icons: '/images/favicon.png'
-}
+// export const metadata = {
+//     title: '.: ASN Berprestasi | Penghargaan Bhakti Karya Husada :.',
+//     description: 'Aplikasi Penghargaan Bhakti Karya Husada',
+//     developer: 'sgt.wibowo@gmail.com',
+//     icons: '/images/favicon.png'
+// }
 
 export default function Page() {
     const [pilih, setPilih] = useState(null)
